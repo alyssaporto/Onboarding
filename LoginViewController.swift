6990cbb37f565c6c666ba565277f82fa6e63b662
Alyssa Porto <app9gv@virginia.edu>
2016-06-01 10:58:11 +0200

    Commit of on boarding app

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Log In View"
 
         // Do any additional setup after loading the view.
     }
